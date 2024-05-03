983c9a26fc678b61be0d01e8b30af87384591d27
Anant <aradhyaanant1@gmail.com>
2024-05-03 21:32:05 +0530

    product model

--- Backend/models/product.js
+++ Backend/models/product.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose";          //this model is created for admin,who will list all the product//
 const productSchema=new mongoose.Schema({
     name:{
         type: String,
@@ -10,7 +10,7 @@ const productSchema=new mongoose.Schema({
         required:[true,'Please enter product price'],
         maxLength:[5,"product name cannot exceed 200 characters"],
     },
-    dewscription:{
+    description:{
         type: String,
         required:[true,"Please enter product description"],
     }, 
@@ -20,16 +20,60 @@ const productSchema=new mongoose.Schema({
     },
     images:[
         {
-            public_id:String,
-            url: String,
+            public_id:{
+                type:String,
+                required:true
+            },
+            url: {
+                type: String,
+                required:true,
+            },
         },
     ],
     category:{
         type: String,
         required:[true,'Please enter product category'],
-        enum:{                      //in enum we restrict the user to some specific values//                        
-            values:["Laptops,electronics,cameras,headphones, food,headphones,home,outdoorzx"],                  
+        enum:{                      //in enum we restrict the user to some specific values,the user cannot enter the products other than the mentioned array//                        
+            values:["Laptops,electronics,cameras,headphones, food,headphones,home,outdoorzx"],                  //similar to drop down menu//
             message:"Please select correct category",
         },
     },
-});
+    Seller:{
+        type: String,
+        required:[true,"Please enter product seller"],
+    },
+    stock:{
+        type: Number,
+        required:[true,"Please enter product stock"],
+    },
+    numOfReviews:{
+        type: Number,
+        default: 0,
+    },
+    reviews:[
+        {
+            user:{                                    //this is that user that has created the review//
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"User",
+                required: true
+            },
+            rating:{
+                type: Number,
+                required:true
+            },
+            comment:{
+                type: String,
+                required: true
+            },
+        },
+    ],
+    user:{                                      //this is that user that has created the product//
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required: true,
+    },    
+},{
+    timestamps: true              //mongooose will automatically create a at what date it is created// 
+}
+);
+export default mongoose.model("Product",productSchema);
