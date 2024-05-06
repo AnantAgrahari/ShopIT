9e6bb6d735959c8bd6c8e553056fae57d03d5ec4
Anant <aradhyaanant1@gmail.com>
2024-05-06 09:55:48 +0530

    commit

--- Backend/controllers/productControllers.js
+++ Backend/controllers/productControllers.js
@@ -1,21 +1,22 @@
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import Product from "../models/product.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
-export const getProducts=async(req,res)=>{
+export const getProducts=catchAsyncErrors(async(req,res)=>{
      const products=await Product.find();     //it will find all the products that are listed in product collection in the database//
 
     res.status(300).json({products,});
-}
+});
 
 
-export const newProduct=async(req,res)=>{
+export const newProduct=catchAsyncErrors(async(req,res)=>{
    const product=await Product.create(req.body);  //create func is used to create a doc of whole model//
 
    res.status(200).json({ product,});
-};
+});
 
 
-export const getProductDetails=async(req,res,next)=>{
+export const getProductDetails=catchAsyncErrors(async(req,res,next)=>{
     const product=await Product.findById(req?.params?.id);  
  
     if(!product)
@@ -23,39 +24,35 @@ export const getProductDetails=async(req,res,next)=>{
        return next(new ErrorHandler('Product not found',404));      //next is a middleware provided by express//
     }
     res.status(200).json({ product,});
- };
+ });
 
 
 
- export const updateProducts=async(req,res)=>{
+ export const updateProducts=catchAsyncErrors(async(req,res)=>{
     let product=await Product.findById(req?.params?.id);  //create func is used to create a doc of whole model//
  
     if(!product)                          //if product is not present//
     {
-        return res.status(404).json({
-       error: "Product not found",
-        });
+        return next(new ErrorHandler('Product not found',404));
     }
 
     product=await Product.findByIdAndUpdate(req?.params?.id,req.body,{
         new:true,            //new keyword will make sure that the product is updated and updated product is shown//
     })
     res.status(200).json({ product,});
- };
+ });
 
 
 
- export const deleteProducts=async(req,res)=>{
+ export const deleteProducts=catchAsyncErrors(async(req,res)=>{
     let product=await Product.findById(req?.params?.id);  //create func is used to create a doc of whole model//
  
     if(!product)                          //if product is not present//
     {
-        return res.status(404).json({
-       error: "Product not found",
-        });
+        return next(new ErrorHandler('Product not found',404));
     }
 
    await product.deleteOne();
 
     res.status(200).json({ message:"Product deleted succesfully",});
- };
+ });
--- Backend/middlewares/errors.js
+++ Backend/middlewares/errors.js
@@ -1,8 +1,29 @@
+import ErrorHandler from  '../utils/errorHandler.js'
 export default (err,req,res,next)=>{
     let error={
         statusCode:err?.statusCode || 500,
         message: err?.message || "Internal Server Error",
     };
+
+
+     //handle invalid mongooose ID error//
+     if(err.name==='CastError')
+     {
+        const message=`Resource not found.Invalid: ${err?.path}`        
+        error=new ErrorHandler(message,404);
+     }
+
+     //handle invalid validation error(if any one of the key is absent in reference to the model schema then it will show the validation error)
+     if(err.name==='ValidationError'){
+        const message=Object.values(err.errors).map((value)=>value.message);
+        error=new ErrorHandler(message,404);
+     }
+     {
+        const message=`Resource not found.Invalid: ${err?.path}`        
+        error=new ErrorHandler(message,404);
+     }
+
+
 
    if(process.env.NODE_ENV==="DEVELOPMENT")
    {
