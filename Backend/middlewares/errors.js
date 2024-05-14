d409108d2b90a9916521ecd4997612f2606a6fc0
Anant <aradhyaanant1@gmail.com>
2024-05-14 17:06:03 +0530

    commit

--- Backend/middlewares/errors.js
+++ Backend/middlewares/errors.js
@@ -16,13 +16,33 @@ export default (err,req,res,next)=>{
      //handle invalid validation error(if any one of the key is absent in reference to the model schema then it will show the validation error)
      if(err.name==='ValidationError'){
         const message=Object.values(err.errors).map((value)=>value.message);
-        error=new ErrorHandler(message,404);
+        error=new ErrorHandler(message,400);
      }
-     {
-        const message=`Resource not found.Invalid: ${err?.path}`        
-        error=new ErrorHandler(message,404);
-     }
+    //  {
+    //     const message=`Resource not found.Invalid: ${err?.path}`        
+    //     error=new ErrorHandler(message,404);
+    //  }
 
+      //handle mongooose duplicate key error//
+      if(err.code===11000)
+      {
+         const message=`Duplicate ${Object.keys(err.keyValue)} entered`;        
+         error=new ErrorHandler(message,400);
+      }
+
+      //handle wrong JWT error//
+      if(err.name==="JsonWebToken"){
+        const message=`JSON Web Token is invalid. Try Again!!!`;
+        error=new ErrorHandler(message,400);
+      }
+
+
+      //Handle expired JWT Error//
+     if(err.name==='TokenExpiredError')
+     {
+        const message=`JSON web token is expired. Try Again!!!`        
+        error=new ErrorHandler(message,400);
+     }
 
 
    if(process.env.NODE_ENV==="DEVELOPMENT")
