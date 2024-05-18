1a1b63c802614b8936ea585bb57d923869a91238
Anant <aradhyaanant1@gmail.com>
2024-05-18 21:21:38 +0530

    commit

--- Backend/controllers/orderConntroller.js
+++ Backend/controllers/orderConntroller.js
@@ -1,4 +1,5 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import Product from "../models/product.js";
 import Order from "../models/order.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
@@ -50,5 +51,66 @@ export const myOrders=catchAsyncErrors(async(req,res,next)=>{
 
 
 
+
+// Get all Orders-  Admin route//
+export const allOrders=catchAsyncErrors(async(req,res,next)=>{
+    const orders=await Order.find();
+ res.status(200).json({
+    orders,
+ });
+});
+
+
+
+
+// update Order with order id-  Admin route//
+export const updateOrder=catchAsyncErrors(async(req,res,next)=>{
+    const order=await Order.findById(req.params.id);
+    if(!order)
+    {
+        return next(new ErrorHandler('No order found with this ID',404));
+    }
+     
+    if(order?.orderStatus==="Delivered"){
+        return next (new ErrorHandler('You have alreday deliveered the product',400));
+    }
+
+    // Update products stock
+    order?.orderItems?.forEach(async(item)=>{
+        const product=await Product.findById(item?.product?.toString());
+        if(!product){
+            return next(new ErrorHandler('No Product found with this ID',404));
+        }
+
+        product.stock=product.stock-item.quantity;              // decreases the product stock by the no. of quantity user has purchased//
+        await product.save({validateBeforeSave: false});
+    });
+
+    order.orderStatus=req.body.status;
+    order.deliveredAt=Date.now();
+    
+    await order.save();
+
+    res.status(200).json({
+    success:true,
+ });
+});
+
+
+
+// delete order by id//
+export const deleteOrder=catchAsyncErrors(async(req,res,next)=>{
+    const order=await Order.findById(req.params.id);        
+
+    if(!order)
+    {
+        return next(new ErrorHandler('No order found with this ID',404))
+    }  
+
+    await order.deleteOne();
+    res.status(200).json({
+        success: true,
+    });
+});
 
 
--- Backend/routes/order.js
+++ Backend/routes/order.js
@@ -1,9 +1,12 @@
 import express from "express"
-import { isAuthenticatedUser } from "../middlewares/auth.js";
-import { getOrderDetails, myOrders, newOrder } from "../controllers/orderConntroller.js";
+import { isAuthenticatedUser,authorizeRoles } from "../middlewares/auth.js";
+import { allOrders, deleteOrder, getOrderDetails, myOrders, newOrder, updateOrder } from "../controllers/orderConntroller.js";
 const router=express.Router();
 
  router.route("/orders/new").post(isAuthenticatedUser,newOrder);
  router.route("/orders/:id").get(isAuthenticatedUser,getOrderDetails);
  router.route("/my/orders").get(isAuthenticatedUser,myOrders);
+ router.route("/admin/orders").get(isAuthenticatedUser,authorizeRoles('admin'),allOrders);
+ router.route("/admin/orders/:id").put(isAuthenticatedUser,authorizeRoles('admin'),updateOrder).delete(isAuthenticatedUser,authorizeRoles('admin'),deleteOrder);
+ 
 export default router;
