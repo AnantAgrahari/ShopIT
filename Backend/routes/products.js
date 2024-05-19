5929da61cd7e8bfcdc867bc83c9ee9548c4c6c85
Anant <aradhyaanant1@gmail.com>
2024-05-19 12:41:54 +0530

    commit

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getProducts, newProduct, getProductDetails, updateProducts,deleteProducts } from "../controllers/productControllers.js";
+import { getProducts, newProduct, getProductDetails, updateProducts,deleteProducts, createProductReview, getProductReviews, deleteReview } from "../controllers/productControllers.js";
 import { authorizeRoles,isAuthenticatedUser } from "../middlewares/auth.js";
 const router=express.Router();   //will enable the routes function//
 
@@ -11,6 +11,13 @@ router.route("/products/:id").get(getProductDetails);
 
 router.route("/admin/products/:id").put(isAuthenticatedUser,authorizeRoles('admin'),updateProducts);        // Only admins can update or delete the products//
 router.route("/admin/products/:id").delete(isAuthenticatedUser,authorizeRoles('admin'),deleteProducts);
+
+router.route("/reviews").get(isAuthenticatedUser,getProductReviews).put(isAuthenticatedUser,createProductReview); 
+
+router.route("/admin/reviews").delete(isAuthenticatedUser,authorizeRoles('admin'),deleteReview);
+
+
+
 export default router;
 
 //max. of this routes are protected routes and only authenticated users can access this and for that middleware is created//
