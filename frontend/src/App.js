aec0b829aaf221002ebbda573a08f38bee40baba
Anant <aradhyaanant1@gmail.com>
2024-05-22 00:39:09 +0530

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/layout/Header";
+import Footer from "./components/Footer";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
