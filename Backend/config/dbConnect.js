7bc4601c56a173355253c0bf4a1ac8194f42c681
Anant <aradhyaanant1@gmail.com>
2024-06-17 12:02:10 +0530

    commit

--- Backend/config/dbConnect.js
+++ Backend/config/dbConnect.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config ({path:"backend/config/config.env"});
 export const connectDatabase=()=>{
 
     let DB_URI="";
 
     if(process.env.NODE_ENV==="DEVELOPMENT") DB_URI=process.env.DB_LOCAL_URI;
     if(process.env.NODE_ENV==="PRODUCTION") DB_URI=process.env.DB_URI;
-    mongoose.connect(DB_URI).then((con)=>{
+    mongoose.connect('mongodb+srv://anant:2jvw8VHYKdCX9BeU@cluster0.3h6loaz.mongodb.net/').then((con)=>{
         console.log(`MongoDb Database connected with Host: ${con?.connection?.host}`);
     }) ;
 };
--- Backend/controllers/productControllers.js
+++ Backend/controllers/productControllers.js
@@ -7,6 +7,7 @@ import ErrorHandler from "../utils/errorHandler.js";
 
 
 export const getProducts=catchAsyncErrors(async(req,res,next)=>{
+   console.log("fshdhyfdjhyfhjfhjfghvnhv");
     const resPerPage=4;
     const apiFilters=new APIFilters(Product,req.query).search().filters();    //searches the product with just a keyword//
     let products=await apiFilters.query;      //same here also//
@@ -14,7 +15,7 @@ export const getProducts=catchAsyncErrors(async(req,res,next)=>{
 
     apiFilters.pagination(resPerPage);
     products=await apiFilters.query.clone();   // we are executing it for the 2nd time so we have to clone it//
-
+   console.log(products);
     res.status(200).json({resPerPage,filteredProductsCount,products}); 
 });
 
